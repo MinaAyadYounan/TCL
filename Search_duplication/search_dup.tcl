3c9bbf24dac16c6777f59f16a5386b812940319a
Mina Ayad <145565844+MinaAyadYounan@users.noreply.github.com>
2025-08-01 23:42:30 +0300

    Update search_dup.tcl

--- Search_duplication/search_dup.tcl
+++ Search_duplication/search_dup.tcl
@@ -10,3 +10,19 @@ proc search_dup {a} {
 }
 
 puts [search_dup {A B C B}]
+
+proc search_dup_2 {a} {
+    for {set i 0} {$i < [llength $a]} {incr i 1} {
+      set element [lindex $a $i]
+    for {set j [expr $i + 1]} {$j < [llength $a]} {incr j 1} {
+      set elej [lindex $a $j]
+      if {[string equal $element $elej]} {
+        return $element;
+      }
+      
+    }      
+}
+  return 0;
+}
+puts "Method 2 :"
+puts [search_dup_2 {A B C B}]
